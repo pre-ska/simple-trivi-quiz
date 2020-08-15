37642139377a79931454a8004638863b2244ef1d
preska <wolfwar@outlook.com>
2020-08-15 10:55:47 +0200

    init

---
+++ src/components/Question.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Question = ({ question }) => {
+  return (
+    <div>
+      <h2 dangerouslySetInnerHTML={{ __html: question.question }}></h2>
+      {question.answerChoices.map((choice, index) => (
+        <div key={index} className="choice-container">
+          <p className="choice-prefix">{index + 1}</p>
+          <p
+            className="choice-text"
+            dangerouslySetInnerHTML={{ __html: choice }}></p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Question;
